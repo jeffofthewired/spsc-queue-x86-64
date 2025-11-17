d814bc8dd429531aa4469ab920a865b7247e062c
mt-ra <jeff.j.lu2@gmail.com>
2025-11-17 12:25:24 +1100

    implemented push

--- spsc_queue/member_functions/4fullEv.asm
+++ spsc_queue/member_functions/4fullEv.asm
@@ -19,10 +19,19 @@ _ZN10spsc_queue4fullEv:
         je      case_maybe_full
 
 case_not_full:
-        mov     rax, 1
+        mov     rax, 0
         ret
 
 case_maybe_full:
-        ; TODO: finish implementing
+        ; update the cached_pop_cursor
+        mov     r9, [rdi+o_pop_cursor]
+        mov     [rdi+o_cached_pop_cursor], r9
+        ; now do the same comparison
+        mov     r8, rdx
+        sub     r8, r9
+        cmp     r8, rcx
+        jne     case_not_full
 
+        ; certainly full
+        mov     rax, 1
         ret
